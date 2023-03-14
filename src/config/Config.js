19b249bc147e25b55a1c380139f6803affade29c
johannes <johannes.muljana@tafensw.edu.au>
2023-03-14 21:23:39 +1100

    added project and firebase

---
+++ src/config/Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBRLU8My3cFx7Q9j05RlpGcADE2jSpHKvs",
+    authDomain: "classproject-937e8.firebaseapp.com",
+    projectId: "classproject-937e8",
+    storageBucket: "classproject-937e8.appspot.com",
+    messagingSenderId: "669600973558",
+    appId: "1:669600973558:web:72e86eca25137e80d622d7"
+};
+  
