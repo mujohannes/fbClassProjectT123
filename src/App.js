c56592602e4d43844b0350c57cc961edca4c99e2
johannes <johannes.muljana@tafensw.edu.au>
2023-03-21 20:48:55 +1100

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,21 @@ import { initializeApp } from 'firebase/app';
 
 import './App.css';
 
-const FirebaseApp = initializeApp( firebaseConfig )
+const FirebaseApp = initializeApp(firebaseConfig)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Hello React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+
+      <button type="button" class="btn btn-link">Link</button>
     </div>
   );
 }
