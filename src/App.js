d71cf4e1fcbec1be5bcffb30b7f30feebe3f2446
johannes <johannes.muljana@tafensw.edu.au>
2023-03-28 21:28:47 +1100

    added route to /home

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home/> } />
+        <Route path="/home" element={ <Home/> } />
         <Route path="/about" element={ <About/> } />
         <Route path="/contact" element={ <Contact/> } />
       </Routes>
