dd8a09b5e33f459595233e29436373a5659f99f9
johannes <johannes.muljana@tafensw.edu.au>
2023-05-16 20:26:31 +1000

    redirect user to home from signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,6 +7,7 @@ import Button from "react-bootstrap/Button"
 import { useState, useEffect, useContext } from "react"
 import { FBAuthContext } from "../contexts/FBAuthContext"
 import { createUserWithEmailAndPassword } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
 
 export function Signup ( props ) {
   const [ email, setEmail ] = useState("")
@@ -15,6 +16,7 @@ export function Signup ( props ) {
   const [ validPassword, setValidPassword ] = useState( false )
 
   const FBAuth = useContext( FBAuthContext)
+  const navigate = useNavigate()
 
   useEffect( () => {
     if( email.indexOf('@') > 0 ) {
@@ -38,8 +40,9 @@ export function Signup ( props ) {
     createUserWithEmailAndPassword( FBAuth, email, password )
     .then( ( user ) => {
       // user is created in Firebase
-      console.log(user)
+      // console.log(user)
       // alert user that account has been created
+      navigate("/")
     })
     .catch( (error) => {
       console.log( error.code, error.message )
