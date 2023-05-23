fc4533eb0a0fa0edc9cbb71172f714f3b0cca4c6
johannes <johannes.muljana@tafensw.edu.au>
2023-05-23 21:10:39 +1000

    adding storage context in app and home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 // import firestore
 import { getFirestore} from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 import { useState } from 'react'
 
 import './App.css';
@@ -20,10 +21,12 @@ import { Routes, Route } from 'react-router-dom';
 import { NavContext } from './contexts/NavContext'
 import { FBAuthContext } from './contexts/FBAuthContext';
 import { FBDbContext } from './contexts/FBDbContext'
+import { FBStorageContext } from './contexts/FBStorageContext';
 
 const FirebaseApp = initializeApp(firebaseConfig)
 const FirebaseAuth = getAuth(FirebaseApp)
 const FirebaseDB = getFirestore(FirebaseApp)
+const FirebaseStorage = getStorage(FirebaseApp)
 
 const NavRoutes = [
   { name: "Home", goto: "/" },
@@ -63,6 +66,7 @@ function App() {
       </NavContext.Provider>
       <FBAuthContext.Provider value={FirebaseAuth}>
       <FBDbContext.Provider value={FirebaseDB}>
+      <FBStorageContext.Provider value={FirebaseStorage}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
@@ -72,6 +76,7 @@ function App() {
           <Route path="/signin" element={<Signin />} />
           <Route path="/signout" element={ <SignOut/> } />
         </Routes>
+      </FBStorageContext.Provider>
       </FBDbContext.Provider>
       </FBAuthContext.Provider>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 import { useContext, useEffect, useState } from 'react';
 import { collection, getDocs } from "firebase/firestore";
+import { ref } from "firebase/storage";
 
 import { FBDbContext } from '../contexts/FBDbContext';
+import { FBStorageContext } from '../contexts/FBStorageContext';
 
 export function Home () {
     const[ data, setData ] = useState([])
 
     const FBDb = useContext(FBDbContext)
+    const FBStorage = useContext( FBStorageContext )
 
     const getData = async () => {
         // get data from firestore collection called "books"
@@ -37,7 +41,11 @@ export function Home () {
     const Columns = data.map( (book, key) => {
         return(
             <Col md="4" key={key}>
-                <h3>{book.title}</h3>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>{book.title}</Card.Title>
+                    </Card.Body>
+                </Card>
             </Col>
         )
     })
